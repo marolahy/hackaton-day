467585938b8c10f754121bc4a54433a865c505c3
marolahy <marolahy@gmail.com>
2020-11-11 16:37:24 -0500

    mise en place plateforme docker

---
+++ auth-server/src/migration/1605125389489-CreateAdminUser.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAdminUser1605125389489 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
