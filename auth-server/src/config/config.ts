3fedfa8c79197434812b5b1030999601adb8bd2b
marolahy <marolahy@gmail.com>
2020-11-11 18:47:08 -0500

    creation de authentification service

---
+++ auth-server/src/config/config.ts
@@ -0,0 +1,3 @@
+export default {
+    jwtSecret: "TYGUIGGI@@@64564"
+};
--- auth-server/src/migration/1605125389489-CreateAdminUser.ts
+++ auth-server/src/migration/1605125389489-CreateAdminUser.ts
@@ -1,8 +1,16 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {getRepository, MigrationInterface, QueryRunner} from "typeorm";
+import {User} from "../entity/User";
 
 export class CreateAdminUser1605125389489 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        let user = new User();
+        user.username = "admin";
+        user.password = "admin";
+        user.hashPassword();
+        user.role = "ADMIN";
+        const userRepository = getRepository(User);
+        await userRepository.save(user);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
